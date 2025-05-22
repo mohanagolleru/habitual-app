da882824bc74f817d965766686bffc96394cd4e3
Yasodhar G <yasodhargolleru@gmail.com>
2025-05-22 22:00:04 +0000

    i dont like placing app name Habitual

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -13,21 +13,26 @@ interface AppHeaderProps {
 export function AppHeader({ onOpenAddHabitDialog }: AppHeaderProps) {
   return (
     <header className="py-4 border-b">
-      <div className="container mx-auto flex items-center justify-between">
-        <Link href="/" className="text-3xl font-bold text-primary">
+      <div className="container mx-auto flex items-center">
+        <div className="flex-1">
+          {/* Left spacer, can be used for other elements in the future */}
+        </div>
+        <Link href="/" className="text-3xl font-bold text-primary text-center">
           Habitual
         </Link>
-        <div className="flex items-center gap-4">
-          <Link href="/heatmap" passHref>
-            <Button variant="outline" size="lg">
-              <LayoutGrid className="mr-2 h-5 w-5" />
-              Heatmap
+        <div className="flex-1 flex justify-end">
+          <div className="flex items-center gap-4">
+            <Link href="/heatmap" passHref>
+              <Button variant="outline" size="lg">
+                <LayoutGrid className="mr-2 h-5 w-5" />
+                Heatmap
+              </Button>
+            </Link>
+            <Button onClick={onOpenAddHabitDialog} variant="default" size="lg">
+              <PlusSquare className="mr-2 h-5 w-5" />
+              Add New Habit
             </Button>
-          </Link>
-          <Button onClick={onOpenAddHabitDialog} variant="default" size="lg">
-            <PlusSquare className="mr-2 h-5 w-5" />
-            Add New Habit
-          </Button>
+          </div>
         </div>
       </div>
     </header>
