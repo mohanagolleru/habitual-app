23887e4e40a8cea9e494bc20fde3b2575f4fd1be
Yasodhar G <yasodhargolleru@gmail.com>
2025-05-22 23:30:22 +0000

    i want equal spacing between  Habitual, heatmap and add new habit buttton to be center aligned and equally spaced

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -14,31 +14,24 @@ interface AppHeaderProps {
 export function AppHeader({ onOpenAddHabitDialog }: AppHeaderProps) {
   return (
     <header className="py-3 border-b">
-      <div className="container mx-auto flex items-center">
-        <div className="flex-1">
-          {/* Left spacer, can be used for other elements in the future */}
-        </div>
-        <Link href="/" className="text-3xl font-bold text-primary text-center">
+      <div className="container mx-auto flex items-center justify-center gap-6">
+        <Link href="/" className="text-3xl font-bold text-primary">
           Habitual
         </Link>
-        <div className="flex-1 flex justify-end pr-20"> {/* Changed pr-32 to pr-20 */}
-          <div className="flex items-center gap-4">
-            <Link href="/heatmap" passHref>
-              <Button variant="outline" size="lg">
-                <LayoutGrid className="mr-2 h-5 w-5" />
-                Heatmap
-              </Button>
-            </Link>
-            <Button
-              onClick={onOpenAddHabitDialog}
-              size="lg"
-              className="bg-[#ADFF2F] hover:bg-[#98e61a] text-black"
-            >
-              <PlusSquare className="mr-2 h-5 w-5" />
-              Add New Habit
-            </Button>
-          </div>
-        </div>
+        <Link href="/heatmap" passHref>
+          <Button variant="outline" size="lg">
+            <LayoutGrid className="mr-2 h-5 w-5" />
+            Heatmap
+          </Button>
+        </Link>
+        <Button
+          onClick={onOpenAddHabitDialog}
+          size="lg"
+          className="bg-[#ADFF2F] hover:bg-[#98e61a] text-black"
+        >
+          <PlusSquare className="mr-2 h-5 w-5" />
+          Add New Habit
+        </Button>
       </div>
     </header>
   );
