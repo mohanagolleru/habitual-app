f154816349069a31c4475596a9ce57f31209d774
Yasodhar G <yasodhargolleru@gmail.com>
2025-05-22 23:37:44 +0000

    add new habit button shopould be center alighned to last column

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -14,24 +14,26 @@ interface AppHeaderProps {
 export function AppHeader({ onOpenAddHabitDialog }: AppHeaderProps) {
   return (
     <header className="py-3 border-b">
-      <div className="container mx-auto flex items-center justify-center gap-6">
+      <div className="container mx-auto flex items-center justify-between px-6">
         <Link href="/" className="text-3xl font-bold text-primary">
           Habitual
         </Link>
-        <Link href="/heatmap" passHref>
-          <Button variant="outline" size="lg">
-            <LayoutGrid className="mr-2 h-5 w-5" />
-            Heatmap
+        <div className="flex items-center gap-4 pr-12">
+          <Link href="/heatmap" passHref>
+            <Button variant="outline" size="lg">
+              <LayoutGrid className="mr-2 h-5 w-5" />
+              Heatmap
+            </Button>
+          </Link>
+          <Button
+            onClick={onOpenAddHabitDialog}
+            size="lg"
+            className="bg-[#ADFF2F] hover:bg-[#98e61a] text-black"
+          >
+            <PlusSquare className="mr-2 h-5 w-5" />
+            Add New Habit
           </Button>
-        </Link>
-        <Button
-          onClick={onOpenAddHabitDialog}
-          size="lg"
-          className="bg-[#ADFF2F] hover:bg-[#98e61a] text-black"
-        >
-          <PlusSquare className="mr-2 h-5 w-5" />
-          Add New Habit
-        </Button>
+        </div>
       </div>
     </header>
   );
