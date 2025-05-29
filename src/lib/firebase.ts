8d5f233150ffc8a72ffc3f5f5e06cdb5f1ccb1d7
Mohana Siva Sai Yasodhar Golleru <mohanasi@buffalo.edu>
2025-05-28 23:44:13 -0400

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,9 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBGXb8sNhHTmRmhqYA7B_xp9e5juneFPis",
-  authDomain: "habitual-h31p7.firebaseapp.com",
-  projectId: "habitual-h31p7",
-  storageBucket: "habitual-h31p7.appspot.com", // Corrected from .firebasestorage.app to .appspot.com
-  messagingSenderId: "455096463071",
-  appId: "1:455096463071:web:6f4735235f196ebc68036a"
+  // Your web app's Firebase configuration
 };
 
 // Initialize Firebase
